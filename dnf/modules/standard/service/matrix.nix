c2cb5b0d95c37ab47ecb63940e5fd9972bdbd934
Darkone Linux <darkone@darkone.yt>
2025-11-14 19:23:50 -0300

    Matrix module

---
+++ dnf/modules/standard/service/matrix.nix
@@ -0,0 +1,110 @@
+# A matrix (synapse) server. (WIP)
+
+{
+  lib,
+  pkgs,
+  config,
+  network,
+  ...
+}:
+let
+  inherit network;
+  cfg = config.darkone.service.matrix;
+  srv = config.services.matrix-synapse;
+in
+{
+  options = {
+    darkone.service.matrix.enable = lib.mkEnableOption "Enable matrix (synapse) service";
+    darkone.service.matrix.domainName = lib.mkOption {
+      type = lib.types.str;
+      default = "matrix";
+      description = "Domain name for matrix web client, registered in network configuration";
+    };
+    darkone.service.matrix.appName = lib.mkOption {
+      type = lib.types.str;
+      default = "Matrix";
+      description = "Default title for matrix service";
+    };
+  };
+
+  config = lib.mkMerge [
+    {
+      # Darkone service: httpd + dnsmasq + homepage registration
+      darkone.system.services.service.matrix = {
+        inherit (cfg) domainName;
+        displayName = "Matrix";
+        description = "Communication solution";
+        persist.dirs = [ srv.settings.server_name ];
+        proxy.servicePort = (builtins.elemAt srv.settings.listeners 0).port;
+      };
+    }
+
+    (lib.mkIf cfg.enable {
+
+      # Darkone service: enable
+      darkone.system.services = {
+        enable = true;
+        service.matrix.enable = true;
+      };
+
+      # Tools
+      #environment.systemPackages = with pkgs; [ ];
+
+      # Matrix DB
+      services.postgresql = {
+        enable = true;
+        initialScript = pkgs.writeText "synapse-init.sql" ''
+          CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+          CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+            TEMPLATE template0
+            LC_COLLATE = "C"
+            LC_CTYPE = "C";
+        '';
+      };
+
+      # Matrix server
+      services.matrix-synapse = {
+        enable = true;
+        configureRedisLocally = false; # TODO: true
+        # plugins = with config.services.matrix-synapse.package.plugins; [
+        #   matrix-synapse-ldap3
+        #   matrix-synapse-pam
+        # ];
+        settings = {
+          server_name = network.domain;
+          enable_metrics = false;
+          enable_registration = false;
+          dynamic_thumbnails = false;
+          suppress_key_server_warning = true;
+          listeners = [
+            {
+              port = 8008;
+              bind_addresses = [ "::1" ];
+              type = "http";
+              tls = false;
+              x_forwarded = true;
+              resources = [
+                {
+                  names = [
+                    "client"
+                    "federation"
+                  ];
+                  compress = false;
+                }
+              ];
+            }
+          ];
+          # database = {
+          #   name = "psycopg2"; # PG python driver
+          #   args = {
+          #     user = "matrix-synapse";
+          #     database = "matrix-synapse";
+          #     cp_min = 5;
+          #     cp_max = 10;
+          #   };
+          # };
+        };
+      };
+    })
+  ];
+}
