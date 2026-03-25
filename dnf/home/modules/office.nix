a29245379ec1e84ae7df8b83bd596e44cea4aaa0
Darkone Linux <darkone@darkone.yt>
2026-03-25 10:13:06 -0200

    Bugs in LogSeq new version -> stable

--- dnf/home/modules/office.nix
+++ dnf/home/modules/office.nix
@@ -7,6 +7,7 @@
   network,
   inputs,
   pkgs,
+  pkgs-stable,
   host,
   ...
 }:
@@ -260,7 +261,7 @@ in
       (mkIf cfg.enableTools iotas) # Simple note taking with mobile-first design and Nextcloud sync
       (mkIf cfg.enableTools snapshot) # Webcam
       (mkIf cfg.enableProductivity obsidian)
-      (mkIf cfg.enableProductivity logseq)
+      (mkIf cfg.enableProductivity pkgs-stable.logseq)
       (mkIf cfg.enableBrave brave)
       (mkIf cfg.enableSecurity keepassxc)
       (mkIf cfg.enableSecurity keepmenu) # Dmenu/Rofi frontend for Keepass databases
