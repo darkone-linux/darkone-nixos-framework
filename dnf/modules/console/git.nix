a46dbd590f36c206142a26ff6477122a01cb1414
Darkone Linux <darkone@darkone.yt>
2026-05-12 23:20:55 -0200

    Added GH, removed git tre

--- dnf/modules/console/git.nix
+++ dnf/modules/console/git.nix
@@ -49,9 +49,6 @@ in
           hist =
             ''log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn]''
             + ''%Creset %s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
-          tre =
-            ''log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd)''
-            + ''%C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
           tree =
             "log --graph --abbrev-commit --decorate "
             + "--format=format:'%C(bold blue)%h%C(reset) -"
