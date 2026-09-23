53b005dcd95cf03d6209b79268a259ba8099fe04
Darkone Linux <darkone@darkone.yt>
2026-09-23 13:24:30 -0200

    chore(deps): fleet-update v0.7.1

--- pkgs/fleet-update/package.nix
+++ pkgs/fleet-update/package.nix
@@ -38,13 +38,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "fleet-update";
-  version = "0.6.1";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "darkone-linux";
     repo = "dnf-fleet-update";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-FnG8aaBT0s9TF17TjfHosc0tTlq9wXXLvAx10gHfzDQ=";
+    hash = "sha256-sMfGtZUf9YeHOfVy0rD/RbirUdaaXowB3LSscE2pt7g=";
   };
 
   # Fixed-output: `bun install` needs the network. `--os`/`--cpu` wildcards
